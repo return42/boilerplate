cdf191d1d9d7a39dcdaa7b5e941aa9ada5862f0f
Markus Heiser <markus.heiser@darmarit.de>
2018-09-28 15:31:04 +0200

    setup.sh: normalize and add hooks

--- utils/site-bash/setup.sh
+++ utils/site-bash/setup.sh
@@ -20,120 +20,29 @@ cfg_msg() {
     echo -e "${BYellow}CFG:${_color_Off} $*" >&2
 }
 
-# SCRIPT_FOLDER: Ordner mit den Skripten für die Setups
-#
-#SCRIPT_FOLDER=${REPO_ROOT}/scripts
 
-# TEMPLATES: Ordner in dem die vorlagen für die Setups zu finden sind
-#
-#TEMPLATES="${REPO_ROOT}/templates"
-
-# CACHE: Ordner in dem die Downloads und Builds gecached werden
-#
-CACHE=${REPO_ROOT}/cache
-
-# CONFIG: Ordner unter dem die Konfiguration eines Hosts gesichert werden soll
-# Das wird nicht hier gesetzt sondern in der .config Datei (templates/do_config)
-#
-# CONFIG="${REPO_ROOT}/hostSetup/$(hostname)"
-
-# WWW_USER: Benutzer für die Prozesse des WEB-Servers
-#
-#WWW_USER=www-data
-
-# WWW_FOLDER: Ordner in dem die Resourcen des WEB-Servers liegen
-#
-#WWW_FOLDER=/var/www
-
-# =========
-# toolchain
-# =========
-
-# THREE_WAY_MERGE_CMD: Kommando oder Funktion mit der ein (interaktives)
-# drei-Wege Merge gemacht werden kann. Das Kommando muss die vier Argumente für
-# Dateinamen entgegennehmen.
-#
-#    $THREE_WAY_MERGE_CMD {mine} {yours} {ancestor} {merged}
-#
-#THREE_WAY_MERGE_CMD=merge3FilesWithEmacs
-
-# MERGE_CMD: Kommando oder Funktion mit der ein (interaktiver) Merge gemacht
-# werden kann. Das Kommando muss die drei Argumente für Dateinamen
-# entgegennehmen.
-#
-#     $MERGE_CMD {file_a} {file_b} {merged}
-#
-#MERGE_CMD=merge2FilesWithEmacs
-
-# DIFF_CMD: Kommando oder Funktion mit der ein diff angezeigt werden soll. Im
-# Default wird ``colordiff`` verwendet, wenn das nicht vorhanden ist, dann wird
-# das ganz normale ``diff`` verwendet.
-#
-#DIFF_CMD=colordiff
-
-# =====
-# SAMBA
-# =====
-
-# SAMBA_SERVER=127.0.0.1
-
-# =====================
-# Debian's Apache Setup
-# =====================
-
-# APACHE_SETUP="/etc/apache2"
-# APACHE_SITES_AVAILABE="${APACHE_SETUP}/sites-available"
-# APACHE_MODS_AVAILABE="${APACHE_SETUP}/mods-available"
-# APACHE_CONF_AVAILABE="${APACHE_SETUP}/conf-available"
-
-
-# Debian's OpenLDAP Setup
-# =======================
-
-# LDAP_SERVER="myserver"
-# LDAP_SSL_PORT=636
-# OPENLDAP_USER=openldap
-# SLAPD_DBDIR=/var/lib/ldap
-# SLAPD_CONF="/etc/ldap/slapd.d"
-
-# =======
-# Firefox
-# =======
-
-# FFOX_GLOBAL_EXTENSIONS=/usr/lib/firefox-addons/extensions
-
-# =====
-# GNOME
-# =====
-
-# GNOME_APPL_FOLDER=/usr/share/applications
+# setup's pre hook
+if declare -F hook_load_setup_pre >/dev/null
+then
+    hook_load_setup_pre
+fi
 
 if [[ ! -e "${REPO_ROOT}/.config" ]]; then
     cfg_msg "installing ${REPO_ROOT}/.config"
-    cp "${TEMPLATES}/dot_config" "${REPO_ROOT}/.config"
-    chown -R ${SUDO_USER}:${SUDO_USER} "${REPO_ROOT}/.config"
+    cp "$(dirname ${BASH_SOURCE[0]})/setup_dot_config" "${REPO_ROOT}/.config"
+    chown ${SUDO_USER}:${SUDO_USER} "${REPO_ROOT}/.config"
 fi
 
+# source
 source ${REPO_ROOT}/.config
+source ${REPO_ROOT}/utils/site-bash/common.sh
 
-if [[ ! -e "${CONFIG}_setup.sh" ]]; then
-    cfg_msg "missing setup:"
-    cfg_msg "    ${CONFIG}_setup.sh"
-    cfg_msg "Mostly you will edit the CONFIG variable in ${REPO_ROOT}/.config"
-    cfg_msg "which points to your setup::"
-    cfg_msg "    CONFIG=/path/to/my-config/$(hostname)"
-    cfg_msg "Or use::"
-    cfg_msg "    CONFIG=/path/to/my-config/$(hostname) $0 $*"
-    cfg_msg "For more info about setup, read::"
-    cfg_msg "    ${REPO_ROOT}/hostSetup/MEMO.rst"
-    cfg_msg "To NOT continue with defaults press CTRL-C now!"
-    read -n1 $_t -p "** press any [KEY] to continue **"
-    printf "\n"
-else
-    source ${CONFIG}_setup.sh
+# setup's post hook
+if declare -F hook_load_setup_post >/dev/null
+then
+    hook_load_setup_post
 fi
 
-source ${REPO_ROOT}/utils/site-bash/common.sh
 checkEnviroment
 
 # ----------------------------------------------------------------------------
