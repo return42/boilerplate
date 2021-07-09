5a61ee3509dbfc3b0be4e4eb412ae2f9a9362e51
Markus Heiser <markus.heiser@darmarit.de>
2021-07-09 11:37:21 +0200

    [fix] utils/lib.sh: prefix_stdout show cursor (ANSI escape \e[?25h)
    Some piped commands hide the cursor, show cursory when the stream ends.
    
    Most often this is a bug of the command which piped.  The command should not
    hide the cursor when it writes to a pipe.  I have seen this bug with the package
    manager (pacman) from ArchLinux.
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- utils/lib.sh
+++ utils/lib.sh
@@ -78,8 +78,17 @@ required_commands() {
 
 # shellcheck disable=SC2034
 set_terminal_colors() {
-    _colors=8
+    # https://en.wikipedia.org/wiki/ANSI_escape_code
+
+    # CSI (Control Sequence Introducer) sequences
+    _show_cursor='\e[?25h'
+    _hide_cursor='\e[?25l'
+
+    # SGR (Select Graphic Rendition) parameters
     _creset='\e[0m'  # reset all attributes
+
+    # original specification only had 8 colors
+    _colors=8
 
     _Black='\e[0;30m'
     _White='\e[1;37m'
@@ -270,6 +279,8 @@ prefix_stdout () {
     (while IFS= read line; do
         echo -e "${prefix}$line"
     done)
+    # some piped commands hide the cursor, show cursory when the stream ends
+    echo -en "$_show_cursor"
 }
 
 append_line() {
